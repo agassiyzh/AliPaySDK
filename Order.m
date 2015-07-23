4b05eed34aeea6b33ccdf4fb4fc02e33fef20638
Yosemite <agassi21@gmail.com>
2015-07-23 16:20:44 +0800

    2.1.1.1

--- Order.m
+++ Order.m
@@ -15,43 +15,51 @@
     if (self.partner) {
         [discription appendFormat:@"partner=\"%@\"", self.partner];
     }
-	
-    if (self.seller) {
-        [discription appendFormat:@"&seller_id=\"%@\"", self.seller];
-    }
-	if (self.tradeNO) {
+    
+    if (self.tradeNO) {
         [discription appendFormat:@"&out_trade_no=\"%@\"", self.tradeNO];
     }
-	if (self.productName) {
+    
+    if (self.productName) {
         [discription appendFormat:@"&subject=\"%@\"", self.productName];
     }
-	
-	if (self.productDescription) {
+    
+    if (self.productDescription) {
         [discription appendFormat:@"&body=\"%@\"", self.productDescription];
     }
-	if (self.amount) {
+    
+    if (self.amount) {
         [discription appendFormat:@"&total_fee=\"%@\"", self.amount];
     }
+    
     if (self.notifyURL) {
         [discription appendFormat:@"&notify_url=\"%@\"", self.notifyURL];
     }
-	
+    
     if (self.service) {
         [discription appendFormat:@"&service=\"%@\"",self.service];//mobile.securitypay.pay
-    }
-    if (self.paymentType) {
-        [discription appendFormat:@"&payment_type=\"%@\"",self.paymentType];//1
     }
     
     if (self.inputCharset) {
         [discription appendFormat:@"&_input_charset=\"%@\"",self.inputCharset];//utf-8
     }
-    if (self.itBPay) {
-        [discription appendFormat:@"&it_b_pay=\"%@\"",self.itBPay];//30m
+    
+    if (self.paymentType) {
+        [discription appendFormat:@"&payment_type=\"%@\"",self.paymentType];//1
     }
+    
+    if (self.seller) {
+        [discription appendFormat:@"&seller_id=\"%@\"", self.seller];
+    }
+
     if (self.showUrl) {
         [discription appendFormat:@"&show_url=\"%@\"",self.showUrl];//m.alipay.com
     }
+    
+    if (self.itBPay) {
+        [discription appendFormat:@"&it_b_pay=\"%@\"",self.itBPay];//30m
+    }
+
     if (self.rsaDate) {
         [discription appendFormat:@"&sign_date=\"%@\"",self.rsaDate];
     }
